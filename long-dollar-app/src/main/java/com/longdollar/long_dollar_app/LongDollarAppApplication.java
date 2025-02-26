dda7ca63159501006ee64abd70d8c83fbeabad15
Brandon Gatewood <brandongatewood9@gmail.com>
2025-02-26 02:18:13 -0800

    Connected to database

--- long-dollar-app/src/main/java/com/longdollar/long_dollar_app/LongDollarAppApplication.java
+++ long-dollar-app/src/main/java/com/longdollar/long_dollar_app/LongDollarAppApplication.java
@@ -2,12 +2,24 @@ package com.longdollar.long_dollar_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class LongDollarAppApplication {
 
 	public static void main(String[] args) {
+                Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(LongDollarAppApplication.class, args);
 	}
+
+          // This bean runs at startup to check if the app is connected to the database
+    @Bean
+    public CommandLineRunner checkDatabaseConnection() {
+        return args -> {
+            System.out.println("Successfully connected to the database!");
+        };
+    }
 
 }
