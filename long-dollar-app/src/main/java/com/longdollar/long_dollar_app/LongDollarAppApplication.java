87102400888b517e536cf19079a8eb63fc117904
Brandon Gatewood <brandongatewood9@gmail.com>
2025-02-26 00:56:54 -0800

    Spring boot set up

---
+++ long-dollar-app/src/main/java/com/longdollar/long_dollar_app/LongDollarAppApplication.java
@@ -0,0 +1,13 @@
+package com.longdollar.long_dollar_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongDollarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongDollarAppApplication.class, args);
+	}
+
+}
